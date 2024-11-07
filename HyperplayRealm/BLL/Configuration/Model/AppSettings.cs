65a0003372d186256c0be9ec67b150c22ec17ae0
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-11-07 12:26:19 +0300

    More Updates

--- HyperplayRealm/BLL/Configuration/Model/AppSettings.cs
+++ HyperplayRealm/BLL/Configuration/Model/AppSettings.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BLL.Configuration
+namespace BLL.Configuration.Model
 {
     public class AppSettings
     {
         public string? Name { get; set; }
 
-        public string? Description { get; set; }  
+        public string? Description { get; set; }
     }
 }
